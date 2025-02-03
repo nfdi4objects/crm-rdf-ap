d97faf1f3c02da74b9f73feed5b9c424fb1c8cd1
Jakob Voss <voss@gbv.de>
2025-02-03 13:49:16 +0100

    Sample bibliographic data

---
+++ bibliograpic-examples.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# get sample bibliographic data from diverse sources
+
+# Crossref
+curl -s -L -H "Accept: text/turtle" "https://doi.org/10.1126/science.1157784" > examples/crossref.ttl
+
+# Zenodo
+curl -s https://zenodo.org/records/10971391/export/json-ld | npm run -s jsonld2rdf > examples/zenodo.ttl
+
+# Zotero
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_bibliontology&limit=1' > examples/zotero-bibo.rdf
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_dc&limit=1' > examples/zotero-dc.rdf
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_zotero&limit=1' > examples/zotero-zotero.rdf
