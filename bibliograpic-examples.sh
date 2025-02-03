9312ea8a3937f63901cd5c4d4f1c9b1789a1733e
Jakob Voss <voss@gbv.de>
2025-02-03 13:56:17 +0100

    More examples

--- bibliograpic-examples.sh
+++ bibliograpic-examples.sh
@@ -9,6 +9,6 @@ curl -s -L -H "Accept: text/turtle" "https://doi.org/10.1126/science.1157784" > 
 curl -s https://zenodo.org/records/10971391/export/json-ld | npm run -s jsonld2rdf > examples/zenodo.ttl
 
 # Zotero
-curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_bibliontology&limit=20' > examples/zotero-bibo.rdf
-curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_dc&limit=20' > examples/zotero-dc.rdf
-curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_zotero&limit=20' > examples/zotero-zotero.rdf
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_bibliontology&limit=5' > examples/zotero-bibo.rdf
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_dc&limit=5' > examples/zotero-dc.rdf
+curl -s 'https://api.zotero.org/groups/4673379/items?format=rdf_zotero&limit=5' > examples/zotero-zotero.rdf
